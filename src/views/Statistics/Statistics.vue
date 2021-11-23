e8589f379599db7d7e2fce69d4020fc19b002f7c
dahao <18570088982@163.com>
2021-11-23 11:17:55 +0800

    完成统计页面本周、本月、今年的支出和收入计算

--- src/views/Statistics/Statistics.vue
+++ src/views/Statistics/Statistics.vue
@@ -9,7 +9,7 @@
     <div class="chart">
       <div class="caption">
         <span v-if="interval === 'isoWeek'">本周</span>
-        <span v-else-if="interval === 'moth'">本月</span>
+        <span v-else-if="interval === 'month'">本月</span>
         <span v-else>今年</span>
       </div>
       <div class="total">
@@ -50,8 +50,9 @@ import {DropdownMenu, DropdownItem} from 'vant';
 import {Component, Prop} from 'vue-property-decorator';
 import Tabs from '@/components/common/Tabs.vue';
 import dayjs from 'dayjs';
-import isoWeek from 'dayjs/plugin/isoWeek'
-dayjs.extend(isoWeek)
+import isoWeek from 'dayjs/plugin/isoWeek';
+
+dayjs.extend(isoWeek);
 import clone from '@/lib/clone';
 
 Vue.use(DropdownMenu);
@@ -62,7 +63,7 @@ Vue.use(DropdownItem);
 export default class Statistics extends Vue {
   intervalList = [
     {text: '周', value: 'isoWeek'},
-    {text: '月', value: 'moth'},
+    {text: '月', value: 'month'},
     {text: '年', value: 'year'},
   ];
   interval: 'isoWeek' | 'month' | 'year' = 'isoWeek';
@@ -74,14 +75,42 @@ export default class Statistics extends Vue {
   ];
 
   //get
-  get total(){
-    const amounts = [...this.groupByInterval().values()];
-    console.log(amounts)
-    let sum = 0;
-    for (let i= 0; i< amounts.length; i++){
-      sum += amounts[i]
+  get days() {
+    const [year, month] = [dayjs().year(), dayjs().month()];
+    const day = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    if ((year % 4 === 0 && year % 100 !== 0) || (year % 100 === 0 && year % 400 === 0)) {
+      if (month === 1) {
+        return 29;
+      } else {
+        return day[month];
+      }
+    } else {
+      return day[month];
     }
-    return sum
+  }
+
+  get average() {
+    const now = dayjs();
+    switch (this.interval) {
+      case 'isoWeek':
+        return (this.total / [...this.groupByInterval().values()].length).toFixed(2);
+      case 'month':
+        return (this.total / this.days).toFixed(2);
+      case 'year':
+        return(this.total / 12).toFixed(2)
+      default:
+        return 0;
+
+    }
+  }
+
+  get total() {
+    const amounts = [...this.groupByInterval().values()];
+    let sum = 0;
+    for (let i = 0; i < amounts.length; i++) {
+      sum += amounts[i];
+    }
+    return sum;
   }
 
   get targetRecords(): RecordItem[] {
@@ -91,29 +120,69 @@ export default class Statistics extends Vue {
         .filter((item: { createAt: string | number | Date | dayjs.Dayjs | null | undefined }) => dayjs(item.createAt).isSame(now, this.interval));
   }
 
-  groupByWeek(records: RecordItem[]): Map<string, number> {
-    const keys = [ '周一', '周二', '周三', '周四', '周五', '周六','周日'];
+  groupByYear(records: RecordItem[]): Map<string,number>{
+    const keys = ["一月","二月","三月","四月","五月","六月","七月","八月","九月","十月","十一月","十二月"];
     const result = new Map<string,number>();
     let i: number;
-    for (i = 1; i < keys.length+1; i++){
-      result.set(keys[i],0);
+    for (i = 0; i<keys.length; i++){
+      result.set(keys[i],0)
+    }
+    let record: RecordItem
+    for (record of records){
+      const key = keys[dayjs(record.createAt).month()]
+      const amount = result.get(key) as number
+      result.set(key,amount + record.amount)
+    }
+    return result
+  }
+  groupByMonth(records: RecordItem[]): Map<string, number> {
+    const keys: string[] = [];
+    const result = new Map<string, number>();
+    let i: number;
+    for (i = 1; i <= this.days; i++) {
+      keys.push(i.toString());
+    }
+    for (i = 0; i < keys.length; i++) {
+      result.set(keys[i], 0);
+    }
+    let record: RecordItem;
+    for (record of records) {
+      const key = dayjs(record.createAt).date().toString();
+      const amount = result.get(key) as number;
+      result.set(key, amount + record.amount);
+    }
+    return result;
+  }
+
+  groupByWeek(records: RecordItem[]): Map<string, number> {
+    const keys = ['周一', '周二', '周三', '周四', '周五', '周六', '周日'];
+    const result = new Map<string, number>();
+    let i: number;
+    for (i = 1; i < keys.length + 1; i++) {
+      result.set(keys[i], 0);
     }
     let r: RecordItem;
     for (r of records) {
       const key = keys[dayjs(r.createAt).day()];
       const amount = result.get(key) as number;
-      result.set(key,amount + r.amount)
+      result.set(key, amount + r.amount);
     }
-    return result
+    return result;
   }
-  groupByInterval(): Map<string,number>{
-    let result = new Map<string,number>();
-    switch (this.interval){
-      case "isoWeek":
-        result = this.groupByWeek(this.targetRecords)
+
+  groupByInterval(): Map<string, number> {
+    let result = new Map<string, number>();
+    switch (this.interval) {
+      case 'isoWeek':
+        result = this.groupByWeek(this.targetRecords);
+        break;
+      case 'month':
+        result = this.groupByMonth(this.targetRecords);
         break
+      case 'year':
+        result = this.groupByYear(this.targetRecords)
     }
-    return result
+    return result;
   }
 
 }
